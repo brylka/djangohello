1cbbe2e4be574f4429699e9ccc7b4d2b78d27ab5
Bartosz Bryniarski <brylka@jelenia.com>
2025-05-17 12:54:31 +0200

    sobota

--- test_hello.py
+++ test_hello.py
@@ -4,7 +4,7 @@ from django.test import SimpleTestCase
 class SimpleTests(SimpleTestCase):
     def test_home_page_status_code(self):
         response = self.client.get('/')
-        self.assertEqual(response.status_code, 201)
+        self.assertEqual(response.status_code, 200)
 
     def test_home_page_contains_correct_html(self):
         response = self.client.get('/')
